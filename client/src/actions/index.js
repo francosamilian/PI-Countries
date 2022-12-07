799e6a73218a4552c381fefa62a7a60b73f9f237
francosamiliano <francosamilian@gmail.com>
2022-12-07 18:27:53 -0300

    cambio las rutas de las actions con el nuevo deploy del back en railway

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function getCountries() {
     return async function(dispatch) {
-        let countries = await axios.get('https://pi-countries-francosamilian.herokuapp.com/countries'); 
+        let countries = await axios.get('https://pi-countries-production-17a8.up.railway.app//countries'); 
         return dispatch({
             type: 'GET_COUNTRIES',
             payload: countries.data
@@ -12,7 +12,7 @@ export function getCountries() {
 
 export function getActivities() {
     return async function(dispatch) {
-        let activities = await axios.get('https://pi-countries-francosamilian.herokuapp.com/activities');
+        let activities = await axios.get('https://pi-countries-production-17a8.up.railway.app//activities');
         return dispatch({
             type: 'GET_ACTIVITIES',
             payload: activities.data
@@ -44,7 +44,7 @@ export function orderByName(payload) {
 export function getCountryByName(name) {
     return async function(dispatch) {
         try {
-            let country = await axios.get(`https://pi-countries-francosamilian.herokuapp.com/countries?name=${name}`);
+            let country = await axios.get(`https://pi-countries-production-17a8.up.railway.app//countries?name=${name}`);
             return dispatch({
                 type: 'GET_COUNTRY_BY_NAME',
                 payload: country.data,
@@ -59,7 +59,7 @@ export function getCountryByName(name) {
 export function createActivity(payload) {  
     return async function() {  
         try {
-            await axios.post('https://pi-countries-francosamilian.herokuapp.com/activities', payload);
+            await axios.post('https://pi-countries-production-17a8.up.railway.app//activities', payload);
         } catch (e) {
             console.log(e)
         }                                                 
@@ -69,7 +69,7 @@ export function createActivity(payload) {
 export function countryDetail(id) {
     return async function(dispatch) {
         try {
-            let country = await axios.get(`https://pi-countries-francosamilian.herokuapp.com/countries/${id}`);
+            let country = await axios.get(`https://pi-countries-production-17a8.up.railway.app//countries/${id}`);
             return dispatch({
                 type: 'COUNTRY_DETAIL',
                 payload: country.data
